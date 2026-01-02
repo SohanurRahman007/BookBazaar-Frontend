2511888ed5573ac3dc5b347ba2ab92cdb847496c
Sohanur Rahman <sohanuractive007@gmail.com>
2026-01-02 18:58:51 +0600

    store is done

--- src/store/store.ts
+++ src/store/store.ts
@@ -37,3 +37,13 @@ export const store = configureStore({
       },
     }).concat(api.middleware), // add RTK Query middleware
 });
+//setup the rtk query listeners
+setupListeners(store.dispatch);
+
+//create the persistor
+export const persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
