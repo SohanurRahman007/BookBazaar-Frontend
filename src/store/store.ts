760fde2816859a22acf4e17282e27f0f80cf4602
Sohanur Rahman <sohanuractive007@gmail.com>
2026-01-02 18:54:50 +0600

    store setup with middle ware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import storage from "redux-persist/lib/storage";
+
+import {
+  persistReducer,
+  persistStore,
+  PAUSE,
+  REHYDRATE,
+  PURGE,
+  FLUSH,
+  PERSIST,
+  REGISTER,
+} from "redux-persist";
+import userReducer from "./slice/userSlice";
+import { api } from "./api";
+
 // persist configuration for user
 const userPersistConfig = {
   key: "user",
   storage,
   whitelist: ["user", "isEmailVerified", "isLoggedIn"],
 };
+
+// wrap user reducer with persist
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer, // RTK Query API reducer
+    user: persistedUserReducer, // persisted user reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(api.middleware), // add RTK Query middleware
+});
