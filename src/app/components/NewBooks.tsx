94de9f0b86fc2a7d36c1b0d8a3345e637eda61bd
Sohanur Rahman <sohanuractive007@gmail.com>
2026-01-09 23:21:33 +0600

    newly added books header done

--- src/app/components/NewBooks.tsx
+++ src/app/components/NewBooks.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const NewBooks = () => {
-  return <div>NewBooks</div>;
+  return (
+    <section className="bg-gray-50 py-16">
+      <div className="container mx-auto px-4">
+        <h3 className="text-3xl font-bold text-center">Newly Added Books </h3>
+      </div>
+    </section>
+  );
 };
