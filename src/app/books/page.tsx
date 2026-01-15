c4c15f126691bdd70b8dfcdce2f40f40040ea83a
Sohanur Rahman <sohanuractive007@gmail.com>
2026-01-16 00:56:33 +0600

    book page state done

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function page() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCondition, setSelectedCondition] = useState<string[]>([]);
+  const [selectedType, setSelectedType] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+  const [sortOption, setSortOption] = useState("newest");
+
+  const bookPages = 6;
+
   return <div>page</div>;
 }
 
