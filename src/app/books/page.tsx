2407e753a98ef29523ade6739c502d0dda7865e1
Sohanur Rahman <sohanuractive007@gmail.com>
2026-01-16 01:08:05 +0600

    filter toggle function ready

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { set } from "react-hook-form";
 
 function page() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -6,8 +7,27 @@ function page() {
   const [selectedType, setSelectedType] = useState<string[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
   const [sortOption, setSortOption] = useState("newest");
-
   const bookPages = 6;
+
+  const toggleFilter = (section: string, item: string) => {
+    const updateFilter = (prev: string[]) => {
+      prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item];
+    };
+    switch (section) {
+      case "condition":
+        setSelectedCategory(updateFilter);
+        break;
+      case "classType":
+        setSelectedType(updateFilter);
+        break;
+      case "category":
+        setSelectedCategory(updateFilter);
+        break;
+      default:
+        break;
+    }
+    setCurrentPage(1);
+  };
 
   return <div>page</div>;
 }
